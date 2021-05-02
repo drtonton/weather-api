1ff9f59e5ae11476e33aecbcdc5a62c429c2f6b1
Keaton <tkfoster26@gmail.com>
2021-05-02 15:11:47 -0600

    installed jest

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 8000;
 
 const app: Application = express();
 
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
