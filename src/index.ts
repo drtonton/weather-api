c4165e39626aa71319445a8112f8cd97fbcbe880
Keaton <tkfoster26@gmail.com>
2021-05-02 15:17:43 -0600

    ignoring .env, configuring dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express, { Application } from "express";
 
-const PORT = process.env.PORT || 8000;
+require('dotenv').config();
 
+const PORT = process.env.PORT || 8000;
 const app: Application = express();
 
 app.use(express.json());
-
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
+  console.log('process.env', process.env.PORT, process.env.OPEN_WEATHER_API_KEY);
 });
