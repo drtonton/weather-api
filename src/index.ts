73aba67e40cf97a99393293c7d34299de402a06b
Keaton <tkfoster26@gmail.com>
2021-05-02 15:18:09 -0600

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,4 @@ const app: Application = express();
 app.use(express.json());
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
-  console.log('process.env', process.env.PORT, process.env.OPEN_WEATHER_API_KEY);
 });
