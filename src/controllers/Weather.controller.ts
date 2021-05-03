018faf4975d411974c6076d847d14d5a0d7bb571
Keaton <tkfoster26@gmail.com>
2021-05-02 22:03:58 -0600

    receiving weather data

--- src/controllers/Weather.controller.ts
+++ src/controllers/Weather.controller.ts
@@ -1,9 +1,12 @@
-interface TestResponse { // todo: clean this up later
+import { getWeatherData } from '../services/Weather.service';
+
+interface TestResponse {
   message: string;
 }
 
-export default class WeatherController {
+export default class WeatherController { // turn this into a regular fn
   public async getWeatherForLocation(): Promise<TestResponse> {
+    const bro = await getWeatherData('hi im a locatin');
     return {
       message: 'beep boop!',
     };
--- src/services/Weather.service.ts
+++ src/services/Weather.service.ts
@@ -1,7 +1,12 @@
 const axios = require('axios').default;
 
+// const url: string = `api.openweathermap.org/data/2.5/weather?q=London&appid={API key}`;
 
-// const WeatherService
-
-
-// export default WeatherService;
+export async function getWeatherData(location:string) {
+  try {
+    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${process.env.OPEN_WEATHER_API_KEY}`);
+    console.log('PICLES', response);
+  } catch (err) {
+    console.log('THERE WAS AN ERROR', err);
+  }
+}
