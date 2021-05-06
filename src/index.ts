c8ab571937db666622fea65a875eef0c408163d2
Keaton <tkfoster26@gmail.com>
2021-05-05 19:52:41 -0600

    allowing cross origin requests

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express, { Application } from 'express';
+
 import Router from './routers/index.router';
 
 require('dotenv').config();
 
+const cors = require('cors');
 const PORT = process.env.PORT || 8001;
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(Router);
 app.listen(PORT, () => {
   console.log('Server is running on port', PORT);
